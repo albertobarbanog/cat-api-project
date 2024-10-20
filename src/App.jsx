168eaebca4625141641ac7a0e47aef5688044b78
Alberto Barbano <albertobarbanog@gmail.com>
2024-10-19 23:03:49 -0300

    Se agrega ErrorBoundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {HomePage} from './pages/HomePage';
-import {AboutPage} from './pages/AboutPage'; 
-import {NavBar} from './components/NavBar'; // Importa el NavBar
+import { HomePage } from './pages/HomePage';
+import { AboutPage } from './pages/AboutPage';
+import { ErrorBoundary } from './components/ErrorBoundary';
+import { NavBar } from './components/NavBar'; // Importa el NavBar
 
 export const App = () => {
   return (
     <Router>
       <NavBar />
+      <ErrorBoundary>
         <div className='container mt-4'>
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/about' element={<AboutPage />} />{' '}
           </Routes>
         </div>
+      </ErrorBoundary>
     </Router>
   );
 };
---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,29 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error('Error caught in ErrorBoundary:', error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.node.isRequired,
+};
